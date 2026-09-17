cb05fd795f2276bf34c8cfbda357198a9d2a36f1
Paw <paw@pawrequest.com>
2026-09-17 18:01:49 +0100

    2

--- src/pycommence2/config.py
+++ src/pycommence2/config.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
+import platformdirs
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 
 CategoryName = str
+
 
 class Sort(BaseModel):
     field: str
@@ -14,15 +16,15 @@ class PycommenceSettings(BaseSettings):
     sorts: dict[CategoryName, list[Sort]] = {}
 
     @classmethod
-    def from_toml(cls, path: Path) -> "PycommenceSettings":
+    def default(cls):
+        toml_path = Path(platformdirs.user_config_path('pcommence2', 'pawrequest')).resolve() / 'config.toml'
+        toml_path.touch(exist_ok=True)
+        return cls.from_toml(toml_path)
+
+    @classmethod
+    def from_toml(cls, path: Path) -> 'PycommenceSettings':
         import tomllib
 
-        with open(path, "rb") as f:
+        with open(path, 'rb') as f:
             data = tomllib.load(f)
         return cls(**data)
-
-    ...
-
-if __name__ == "__main__":
-    settings = PycommenceSettings.from_toml(Path(r'C:\prdev\amdev\pycommence\data\config.toml'))
-    print(settings)
